1898af0f9b5af5c6596e82e939f4346fe95a655b
marissaridzuan <77322282+marissaridzuan@users.noreply.github.com>
2021-03-09 15:14:00 +0800

    test git push from terminal to heroku main

--- src/example/orb/script.js
+++ src/example/orb/script.js
@@ -3,6 +3,7 @@ import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.124.0/exampl
 import { Rhino3dmLoader } from 'https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/loaders/3DMLoader.js'
 import rhino3dm from 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/rhino3dm.module.js'
 import { RhinoCompute } from 'https://cdn.jsdelivr.net/npm/compute-rhino3d@0.13.0-beta/compute.rhino3d.module.js'
+import * as THREE from '../build/three.module.js';
 
 // reference the definition
 const definitionName = 'test.gh'
@@ -24,45 +25,6 @@ loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/' )
 // create a few variables to store a reference to the rhino3dm library and to the loaded definition
 let rhino, definition, doc
 
-function init() {
-
-    THREE.Object3D.DefaultUp = new THREE.Vector3( 0, 0, 1 )
-
-    // create a scene and a camera
-    scene = new THREE.Scene()
-    scene.background = new THREE.Color(1,1,1)
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
-    camera.position.z = 300
-
-
-    // create the renderer and add it to the html
-    renderer = new THREE.WebGLRenderer( { antialias: true } )
-    renderer.setSize( window.innerWidth, window.innerHeight )
-    document.body.appendChild( renderer.domElement )
-
-    const controls = new OrbitControls( camera, renderer.domElement )
-
-    const directionalLight = new THREE.DirectionalLight( 0xffffff )
-    directionalLight.position.set( 0, 0, 2 )
-    directionalLight.castShadow = true
-    directionalLight.intensity = 2
-    scene.add( directionalLight )
-
-    raycaster = new THREE.Raycaster()
-
-    const loader = new Rhino3dmLoader()
-    loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.13.0/' )
-
-    loader.load( 'studio-test.3dm', function ( object ) {
-
-        document.getElementById('loader').remove()
-        scene.add( object )
-        console.log( object )
-
-    } )
-
-
-}
 
 rhino3dm().then(async m => {
     rhino = m
@@ -198,6 +160,159 @@ function saveByteArray ( fileName, byte ) {
 // BOILERPLATE //
 // declare variables to store scene, camera, and renderer
 let scene, camera, renderer
+
+let container, stats;
+const params = {
+    projection: 'normal',
+    autoRotate: true,
+    reflectivity: 1.0,
+    background: false,
+    exposure: 1.0,
+    gemColor: 'Green'
+};
+let camera, scene, renderer;
+let gemBackMaterial, gemFrontMaterial;
+let hdrCubeRenderTarget;
+
+const objects = [];
+
+init();
+animate();
+
+function init() {
+
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
+    camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera.position.set( 0.0, - 10, 20 * 3.5 );
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0x000000 );
+
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+
+    gemBackMaterial = new THREE.MeshPhysicalMaterial( {
+        map: null,
+        color: 0x0000ff,
+        metalness: 1,
+        roughness: 0,
+        opacity: 0.5,
+        side: THREE.BackSide,
+        transparent: true,
+        envMapIntensity: 5,
+        premultipliedAlpha: true
+        // TODO: Add custom blend mode that modulates background color by this materials color.
+    } );
+
+    gemFrontMaterial = new THREE.MeshPhysicalMaterial( {
+        map: null,
+        color: 0x0000ff,
+        metalness: 0,
+        roughness: 0,
+        opacity: 0.25,
+        side: THREE.FrontSide,
+        transparent: true,
+        envMapIntensity: 10,
+        premultipliedAlpha: true
+    } );
+
+    const manager = new THREE.LoadingManager();
+    manager.onProgress = function ( item, loaded, total ) {
+
+        console.log( item, loaded, total );
+
+    };
+
+    const loader = new OBJLoader( manager );
+    loader.load( 'models/obj/emerald.obj', function ( object ) {
+
+        object.traverse( function ( child ) {
+
+            if ( child instanceof THREE.Mesh ) {
+
+                child.material = gemBackMaterial;
+                const second = child.clone();
+                second.material = gemFrontMaterial;
+
+                const parent = new THREE.Group();
+                parent.add( second );
+                parent.add( child );
+                scene.add( parent );
+
+                objects.push( parent );
+
+            }
+
+        } );
+
+
+    } );
+
+    new RGBELoader()
+        .setDataType( THREE.UnsignedByteType )
+        .setPath( 'textures/equirectangular/' )
+        .load( 'royal_esplanade_1k.hdr', function ( hdrEquirect ) {
+
+            hdrCubeRenderTarget = pmremGenerator.fromEquirectangular( hdrEquirect );
+            pmremGenerator.dispose();
+
+            gemFrontMaterial.envMap = gemBackMaterial.envMap = hdrCubeRenderTarget.texture;
+            gemFrontMaterial.needsUpdate = gemBackMaterial.needsUpdate = true;
+
+            hdrEquirect.dispose();
+
+        } );
+
+    const pmremGenerator = new THREE.PMREMGenerator( renderer );
+    pmremGenerator.compileEquirectangularShader();
+
+    // Lights
+
+    scene.add( new THREE.AmbientLight( 0x222222 ) );
+
+    const pointLight1 = new THREE.PointLight( 0xffffff );
+    pointLight1.position.set( 150, 10, 0 );
+    pointLight1.castShadow = false;
+    scene.add( pointLight1 );
+
+    const pointLight2 = new THREE.PointLight( 0xffffff );
+    pointLight2.position.set( - 150, 0, 0 );
+    scene.add( pointLight2 );
+
+    const pointLight3 = new THREE.PointLight( 0xffffff );
+    pointLight3.position.set( 0, - 10, - 150 );
+    scene.add( pointLight3 );
+
+    const pointLight4 = new THREE.PointLight( 0xffffff );
+    pointLight4.position.set( 0, 0, 150 );
+    scene.add( pointLight4 );
+
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.shadowMap.enabled = true;
+    container.appendChild( renderer.domElement );
+
+    renderer.outputEncoding = THREE.sRGBEncoding;
+
+    stats = new Stats();
+    container.appendChild( stats.dom );
+
+    const controls = new OrbitControls( camera, renderer.domElement );
+    controls.minDistance = 20;
+    controls.maxDistance = 200;
+
+    window.addEventListener( 'resize', onWindowResize );
+
+    const gui = new GUI();
+
+    gui.add( params, 'reflectivity', 0, 1 );
+    gui.add( params, 'exposure', 0.1, 2 );
+    gui.add( params, 'autoRotate' );
+    gui.add( params, 'gemColor', [ 'Blue', 'Green', 'Red', 'White', 'Black' ] );
+    gui.open();
+
+}
 
 function init() {
 
